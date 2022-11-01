16f5c1d961fbeae5cc9165c03b0259eb118fb163
Artem Pitikin <git@kosmotema.dev>
2022-11-01 14:24:37 +0300

    remove escaping of double-quotes inside css code that breaks violentmonkey's editor

--- redfresher.user.js
+++ redfresher.user.js
@@ -6,7 +6,7 @@
 // @license     Mozilla Public License 2.0
 // @downloadURL https://raw.github.com/kosmotema/redfresher/main/redfresher.user.js
 // @homepageURL https://github.com/kosmotema/redfresher
-// @version     2.7.0
+// @version     2.7.1
 // @grant       GM_addStyle
 // @noframes
 // ==/UserScript==
@@ -113,7 +113,7 @@
 
   GM_addStyle(`
 .${BUTTON_CLASS_NAME} {
-  --image: url(\"data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' height='${SIZE}px' viewBox='0 0 24 24' width='${SIZE}px' fill='%23000000'%3E%3Cpath d='M0 0h24v24H0V0z' fill='none'/%3E%3Cpath d='M17.65 6.35c-1.63-1.63-3.94-2.57-6.48-2.31-3.67.37-6.69 3.35-7.1 7.02C3.52 15.91 7.27 20 12 20c3.19 0 5.93-1.87 7.21-4.56.32-.67-.16-1.44-.9-1.44-.37 0-.72.2-.88.53-1.13 2.43-3.84 3.97-6.8 3.31-2.22-.49-4.01-2.3-4.48-4.52C5.31 9.44 8.26 6 12 6c1.66 0 3.14.69 4.22 1.78l-1.51 1.51c-.63.63-.19 1.71.7 1.71H19c.55 0 1-.45 1-1V6.41c0-.89-1.08-1.34-1.71-.71l-.64.65z'/%3E%3C/svg%3E\");
+  --image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' height='${SIZE}px' viewBox='0 0 24 24' width='${SIZE}px' fill='%23000000'%3E%3Cpath d='M0 0h24v24H0V0z' fill='none'/%3E%3Cpath d='M17.65 6.35c-1.63-1.63-3.94-2.57-6.48-2.31-3.67.37-6.69 3.35-7.1 7.02C3.52 15.91 7.27 20 12 20c3.19 0 5.93-1.87 7.21-4.56.32-.67-.16-1.44-.9-1.44-.37 0-.72.2-.88.53-1.13 2.43-3.84 3.97-6.8 3.31-2.22-.49-4.01-2.3-4.48-4.52C5.31 9.44 8.26 6 12 6c1.66 0 3.14.69 4.22 1.78l-1.51 1.51c-.63.63-.19 1.71.7 1.71H19c.55 0 1-.45 1-1V6.41c0-.89-1.08-1.34-1.71-.71l-.64.65z'/%3E%3C/svg%3E");
   
   -webkit-mask-image: var(--image);
   mask-image: var(--image);
